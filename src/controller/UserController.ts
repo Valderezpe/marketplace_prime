d003abaf681471d7767d2685a3b472f1ddc47f0d
valderez <valderezpe@gmail.com>
2023-03-24 23:45:48 -0300

    Api-marketplace, teste Req. Postman.

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,11 @@
-import {Request, Response } from 'express';
+import { Request, Response } from "express";
+import { prisma } from "../database/prisma";
 
-export const createUser = async(req: Request, res: Response) => {
-    const { name, email, password } = req.body
-}
+export const createUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  const user = await prisma.user.create({
+    data: { name, email, password },
+  });
+  return res.json(user);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { router } from "./router";
+
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello" });
-});
 app.listen(4000, () => console.log("Hello World"));
