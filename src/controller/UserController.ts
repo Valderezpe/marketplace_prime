08c9af7ffddfad20793e437d7b095c085f1d2c1c
valderez <valderezpe@gmail.com>
2023-03-25 20:48:03 -0300

    criando crud, bcrypt para password.

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -2,10 +2,44 @@ import { Request, Response } from "express";
 import { prisma } from "../database/prisma";
 
 export const createUser = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, accessName } = req.body;
+
+  const isUserUniqueEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (isUserUniqueEmail) {
+    return res
+      .status(400)
+      .json({ message: "Usuário já cadastrado com este E-mail!" });
+  }
+
+
+
 
   const user = await prisma.user.create({
-    data: { name, email, password },
+
+    data: {
+      name,
+      email,
+      password,
+          Access: {
+            connect: {
+              name: accessName,
+            },
+          },      
+    },
+
   });
   return res.json(user);
 };
+
+export const deleteManyUser = async (req: Request, res: Response) => {
+  await prisma.user.deleteMany();
+  return res.json({ message: "Usuário deletado com sucesso" });
+};
+function hash(password: any, arg1: number) {
+  throw new Error("Function not implemented.");
+}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { createUser } from "./controller/UserController";
+import { createUser, deleteManyUser } from "./controller/UserController";
+import { createAccess, getAllAcesseses } from "./controller/AccessControlle";
 
 export const router = Router();
 
 router.post("/user", createUser);
+router.post("/access", createAccess);
+router.get("/accesses", getAllAcesseses);
+router.delete('/delete-users', deleteManyUser );
